442a15dbe1ff551ec6c44a63681bf672ef4c4096
PanurgRable <112486624+PanurgRable@users.noreply.github.com>
2022-12-12 18:15:13 +0300

    test is passed

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private final Deth8 deth8;
+    @Autowired
+    public Needle7(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
 
     @Override
     public String toString() {
